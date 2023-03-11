17ede9a9778d8e36b02417e94f80e4e44153610c
jesse <jesse.kibaara@student.moringaschool.com>
2023-03-11 16:26:27 +0300

    fizzbuzz code done

--- control_flow.rb
+++ control_flow.rb
@@ -9,52 +9,32 @@ def admin_login(username, password)
   
 
 
-def hows_the_weather(temperature)
-  # your code here
-  case temperature
-  when -Float::INFINITY...40
-    "It's brisk out there!"
-  when 40...65
-    "It's a little chilly out there!"
-  when 65...85
-    "It's perfect out there!"
-  else
-    "It's too dang hot out there!"
-  end
-end
-
-def fizzbuzz(num)
-  # your code here
-  if num % 3 == 0 && num % 5 == 0
-    'FizzBuzz'
-  elsif num % 3 == 0
-    'Fizz'
-  elsif num % 5 == 0
-    'Buzz'
-  else
-    num
-  end
-end
-
-def calculator(operation, num1, num2)
-  # your code here
-  if operation == "+"
-    return num1 + num2
-  elsif operation == "-"
-    return num1 - num2
-  elsif operation == "*"
-    return num1 * num2
-  elsif operation == "/"
-    if num2 == 0
-      return "Error: Cannot divide by zero"
+  def hows_the_weather(temperature)
+    # your code here
+    case temperature
+    when -Float::INFINITY...40
+      "It's brisk out there!"
+    when 40...65
+      "It's a little chilly out there!"
+    when 65...85
+      "It's perfect out there!"
     else
-      return num1 / num2
+      "It's too dang hot out there!"
     end
-  else
-    puts "Invalid operation!"
-    return nil
   end
-end
+
+  def fizzbuzz(num)
+    # your code here
+    if num % 3 == 0 && num % 5 == 0
+      'FizzBuzz'
+    elsif num % 3 == 0
+      'Fizz'
+    elsif num % 5 == 0
+      'Buzz'
+    else
+      num
+    end
+  end
 
 
 
