3073ea4e1568577c08f6c87d4dfe6ef63973f8e6
jesse <jesse.kibaara@student.moringaschool.com>
2023-03-11 16:23:52 +0300

    code to grant access done

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,60 @@
 def admin_login(username, password)
   # your code here
-end
+    if username.downcase == 'admin' && password == '12345'
+      'Access granted'
+    else
+      'Access denied'
+    end
+  end
+  
+
 
 def hows_the_weather(temperature)
   # your code here
+  case temperature
+  when -Float::INFINITY...40
+    "It's brisk out there!"
+  when 40...65
+    "It's a little chilly out there!"
+  when 65...85
+    "It's perfect out there!"
+  else
+    "It's too dang hot out there!"
+  end
 end
 
 def fizzbuzz(num)
   # your code here
+  if num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  if operation == "+"
+    return num1 + num2
+  elsif operation == "-"
+    return num1 - num2
+  elsif operation == "*"
+    return num1 * num2
+  elsif operation == "/"
+    if num2 == 0
+      return "Error: Cannot divide by zero"
+    else
+      return num1 / num2
+    end
+  else
+    puts "Invalid operation!"
+    return nil
+  end
 end
+
+
 
--- spec/control_flow_spec.rb
+++ spec/control_flow_spec.rb
@@ -18,6 +18,7 @@ describe '#admin_login' do
   end
 
 end
+  
 
 describe '#hows_the_weather' do
   
